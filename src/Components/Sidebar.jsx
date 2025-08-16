129d12eeb9ec984274b70627dc50b4c09acc3374
Muzammil-450 <muzammilmukhtar500@gmail.com>
2025-08-16 15:23:02 +0500

    pushed

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -62,7 +62,7 @@ const Sidebar = () => {
         </nav>
       </div>
 
-      {/* Bottom Section */}
+      {/* Bottom Sectidson */}
       <div className="p-4 space-y-4">
         {/* Logout Button */}
         <button
