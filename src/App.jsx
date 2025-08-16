e8e3a865b8055e9631eed122f9ab67b48d1f9593
Muzammil-450 <muzammilmukhtar500@gmail.com>
2025-08-16 15:14:03 +0500

    fix1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { useLocation } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./Components/sidebar"; // Import your Sidebar component
 import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
